4a1f7ac4b2a1fc43d5d93a669601555eefa58fa6
YuriyKortev <kortev.yura@yandex.ru>
2019-10-20 18:35:11 +0300

    circles_fixed//

--- hw3/block5/task5.m
+++ hw3/block5/task5.m
@@ -26,15 +26,25 @@
 function XY = task5 (C1, C2)
   f=@(C1,C2) @(x) [(x(1)-C1(1))^2+(x(2)-C1(2))^2-C1(3)^2;
                    (x(1)-C2(1))^2+(x(2)-C2(2))^2-C2(3)^2];
-             
-  XY=[fsolve(f(C1,C2),[C1(1)-C1(3),C1(2)-C1(3)]).' ,fsolve(f(C1,C2),[C1(1)+C1(3),C1(2)-C1(3)]).'];
-      
+  
+  hold on;
   fi=0:0.01:2*pi;
   x1=C1(3)*cos(fi)+C1(1);
   y1=C1(3)*sin(fi)+C1(2);
   x2=C2(3)*cos(fi)+C2(1);
   y2=C2(3)*sin(fi)+C2(2);
   
-  plot(x1,y1,x2,y2,XY(1,:),XY(2,:),"ro");
-
+  plot(x1,y1,x2,y2);
+  
+  
+  dist=sqrt ((C1(1)-C2(1))^2+(C1(2)-C2(2))^2);
+  
+  if(dist<C1(3)+C2(3) && dist>abs(C1(3)-C2(3)))
+    XY=[fsolve(f(C1,C2),[C1(1)-C1(3),C1(2)-C1(3)]).' ,fsolve(f(C1,C2),[C1(1)+C1(3),C1(2)+C1(3)]).'];
+    plot(XY(1,:),XY(2,:),"ro");  
+  endif
+  if(abs(dist-(C1(3)+C2(3)))<0.0001)
+    XY=fsolve(f(C1,C2),[C1(1)-C1(3),C1(2)-C1(3)]).'; 
+    plot(XY(1,:),XY(2,:),"ro");  
+  endif
 endfunction
